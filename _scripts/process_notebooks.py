36b1dbb3a29294ef2366f6744f3887211984b6a2
Matthew Brett <matthew.brett@gmail.com>
2025-06-10 23:32:04 +0100

    Continue attempts to parse notes

--- _scripts/process_notebooks.py
+++ _scripts/process_notebooks.py
@@ -14,6 +14,12 @@ from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from pathlib import Path
 import re
 
+import docutils.core as duc
+import docutils.nodes as dun
+from docutils.utils import Reporter
+from myst_parser.docutils_ import Parser
+
+_END_DIV_RE = re.compile(r'^\s*(:::+|```+|~~~+)\s*$')
 import jupytext
 
 _JL_JSON_FMT = r'''\
@@ -25,15 +31,25 @@ _JL_JSON_FMT = r'''\
 }}
 '''
 
+_DIV_RE = r'\s*(:::+|```+|~~~+)\s*'
+
+
+_ADM_HEADER = re.compile(
+    rf'''
+    ^{_DIV_RE}
+    \{{\s*(?P<ad_type>\S+)\s*\}}\s*
+    (?P<ad_title>.*)\s*$
+    ''', flags=re.VERBOSE)
+
 
 _EX_SOL_MARKER = re.compile(
-    r'''
+    rf'''
     (?P<newlines>\n*)
-    \s*(```+|:::+|~~~+)\s*
-    \{\s*
+    {_DIV_RE}
+    \{{\s*
     (?P<ex_sol>exercise|solution)-
     (?P<st_end>start|end)
-    \s*\}
+    \s*\}}
     \s*
     (?P<suffix>\S+)?\s*
     \n
@@ -55,6 +71,28 @@ _SOL_MARKED = re.compile(
     flags=re.VERBOSE | re.MULTILINE | re.DOTALL)
 
 
+_END_DIV_RE = re.compile(rf'^{_DIV_RE}$')
+
+
+# https://myst-parser.readthedocs.io/en/latest/syntax/optional.html#syntax-extensions
+MYST_EXTENSIONS = [
+    "amsmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+
+
 def _replace_markers(m):
     st_end = m['st_end']
     if m['ex_sol'] == 'exercise':
@@ -63,8 +101,52 @@ def _replace_markers(m):
     return f'\n<!-- {st_end}-solution -->\n'
 
 
+def get_admonition_lines(nb_text):
+    parser = Parser()
+    doc = duc.publish_doctree(
+        source=nb_text,
+        settings_overrides={
+            "myst_enable_extensions": MYST_EXTENSIONS,
+            'report_level': Reporter.ERROR_LEVEL,
+        },
+        parser=parser)
+    lines = nb_text.splitlines()
+    n_lines = len(lines)
+    admonition_lines = []
+    for admonition in doc.findall(dun.Admonition):
+        start_line = admonition.line - 1
+        following = list(admonition.findall(include_self=False,
+                                            descend=False,
+                                            ascend=True))
+        last_line = following[0].line - 2 if following else n_lines - 1
+        for end_line in range(last_line, start_line + 1, -1):
+            if _END_DIV_RE.match(lines[end_line]):
+                break
+        else:
+            raise ValueError('Could not find end div')
+        admonition_lines.append((start_line, end_line))
+    return admonition_lines
+
+
+_ADM_HEADER = re.compile(
+    r'''
+    ^\s*(:::+|```+|~~~+)\s*
+    \{\s*(?P<ad_type>\S+)\s*\}\s*
+    (?P<ad_title>.*)\s*$
+    ''', flags=re.VERBOSE)
+
+
 def process_notes(nb_text):
-    return nb_text
+    lines = nb_text.splitlines()
+    for first, last in get_admonition_lines(nb_text):
+        m = _ADM_HEADER.match(lines[first])
+        if not m:
+            raise ValueError(f"Cannot get match from {lines[first]}")
+        ad_type, ad_title = m['ad_type'], m['ad_title']
+        suffix = '' if ad_title is None else f': {ad_title}'
+        lines[first] = f"**Start of {ad_type}{suffix}**"
+        lines[last] = f"**End of {ad_type}**"
+    return '\n'.join(lines)
 
 
 def load_process_nb(nb_path, fmt='myst'):
--- _scripts/tests/test_process.py
+++ _scripts/tests/test_process.py
@@ -6,18 +6,33 @@ from pathlib import Path
 
 HERE = Path(__file__).parent
 THERE = HERE.parent
-EG_NB_PATH = HERE / 'eg2.Rmd'
+EG1_NB_PATH = HERE / 'eg.Rmd'
+EG2_NB_PATH = HERE / 'eg2.Rmd'
 
 sys.path.append(str(THERE))
 
 import process_notebooks as pn
 
 
-def test_process():
-    out_nb = pn.load_process_nb(EG_NB_PATH)
+def test_process_eg1():
+    out_nb = pn.load_process_nb(EG1_NB_PATH)
+    cells = out_nb['cells']
+    assert len(cells) == 16
+    assert '**Start of exercise**' in cells[-6]['source'].splitlines()
+    assert '**End of exercise**' in cells[-4]['source'].splitlines()
+    assert '**See page for solution**' in cells[-3]['source'].splitlines()
+
+
+def test_process_eg2():
+    out_nb = pn.load_process_nb(EG2_NB_PATH)
     cells = out_nb['cells']
     assert len(cells) == 10
     ex_cell_lines = cells[-3]['source'].splitlines()
     assert '**Start of exercise**' in ex_cell_lines
     assert '**End of exercise**' in ex_cell_lines
     assert '**See page for solution**' in cells[-2]['source'].splitlines()
+
+
+def test_admonition_finding():
+    nb_text = EG2_NB_PATH.read_text()
+    assert pn.get_admonition_lines(nb_text) == [(24, 34), (126, 130)]
