c85a52adf19431baa23a6584b72b96e3058c4c71
Matthew Brett <matthew.brett@gmail.com>
2025-06-23 16:57:08 +0100

    Another rework of strings page.

--- clean_gender_df_names.py
+++ clean_gender_df_names.py
@@ -1,10 +1,10 @@
 """ A crude way of getting the solution to the string exercise
 """
 
-def get_cleaned(mm_name_series):
+def get_cleaned(name_series):
 
-    # Copy the `country_name` Series
-    clean_mat_mort_names_solution = mm_name_series.copy()
+    # Copy the given Series.
+    clean_mat_mort_names_solution = name_series.copy()
 
     # A dictionary of values. The `.keys()` are the original strings, the `.values()`
     # are the replacement strings
@@ -17,10 +17,9 @@ def get_cleaned(mm_name_series):
 
     # Loop through the keys and values
     for to_replace, replace in zip(replace_dict.keys(), 
-                                replace_dict.values()):
-
-        # Make the replacements
-        clean_mat_mort_names_solution = clean_mat_mort_names_solution.str.replace(to_replace, replace)
-
+                                   replace_dict.values()):
+        # Make the replacements.
+        clean_mat_mort_names_solution = (clean_mat_mort_names_solution
+                                         .str.replace(to_replace, replace))
     # Remove capitalization
     return clean_mat_mort_names_solution.str.lower()
