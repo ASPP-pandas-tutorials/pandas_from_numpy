717e19c0a48b1a86456b948ce8a878343cfa1893
Peter Rush <57416249+pxr687@users.noreply.github.com>
2025-06-23 19:50:39 +0700

    Update hint.py
    Co-authored-by: Matthew Brett <matthew.brett@gmail.com>

--- hint.py
+++ hint.py
@@ -1,4 +1,4 @@
-def hint():
+def hint_1():
     "Prints a hint for the attribute-series exercise"
     print("Try using the `str()` function if you are getting errors regarding the dimensions of `df.shape`...")
 
